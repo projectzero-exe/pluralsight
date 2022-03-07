12649c56fbb65dce91ece5580c37141c729af0b3
enautoDev <granbestia001@gmail.com>
2022-03-06 19:19:18 -0800

    updated ztp file python3

--- enauto1/m2/ztp.py
+++ enauto1/m2/ztp.py
@@ -12,20 +12,20 @@ import cli
 # guaranteed to be unique. Capture the last string from the line
 # which is always the serial number. The output looks like this:
 #   'Processor board ID 9BZ0FXAYK7X'
-print "\n* Capture device serial number"
+print("\n* Capture device serial number")
 sn_text = cli.execute("show version | include ^Processor")
 sn = sn_text.split(" ")[-1]
 
 # Define serial to IP address 2-tuple matching, which provides the
 # unique tunnel and loopback IPs for each device.
-ipaddr_map = {
-    "92Z8YAWE1YO": ("10.0.0.2", "172.16.100.2"),
-    "9G7LDZKE6F4": ("10.0.0.3", "172.16.100.3")
-}
+# ipaddr_map = {
+#     "92Z8YAWE1YO": ("10.0.0.2", "172.16.100.2"),
+#     "9G7LDZKE6F4": ("10.0.0.3", "172.16.100.3")
+# }
 
 # Check the dictionary for a serial number and unpack the addresses
-lb0_ip, tun100_ip = ipaddr_map[sn]
-print "\n* sn {0} -> lb0: {1}, tun100: {2}".format(sn, lb0_ip, tun100_ip)
+# lb0_ip, tun100_ip = ipaddr_map[sn]
+# print "\n* sn {0} -> lb0: {1}, tun100: {2}".format(sn, lb0_ip, tun100_ip)
 
 # Define the config commands necessary for day 0 setup. In summary:
 #  1. Configure a unique hostname
@@ -44,20 +44,7 @@ config_cmds = [
     "cdp run",
     "line vty 0 4",
     "login local",
-    "transport input ssh",
-    "interface Loopback0",
-    "ip address {} 255.255.255.255".format(lb0_ip),
-    "ip ospf 1 area 0",
-    "interface Tunnel100",
-    "description DMVPN SPOKE TUNNEL",
-    "ip address {} 255.255.255.0".format(tun100_ip),
-    "cdp enable",
-    "ip nhrp network-id 100",
-    "ip nhrp nhs dynamic nbma connect.njrusmc.net multicast",
-    "ip ospf network point-to-multipoint",
-    "ip ospf 1 area 0",
-    "tunnel source GigabitEthernet1",
-    "tunnel mode gre multipoint"
+    "transport input ssh"
 ]
 
 print "\n* Performing ZTP configuration"
@@ -69,10 +56,9 @@ print "\n* Performing ZTP verification"
 show_cmds = [
     "show ip ssh",
     "show ip interface brief",
-    "show dhcp lease",
-    "show ip ospf interface brief"
+    "show dhcp lease"
 ]
 
 for show_cmd in show_cmds:
-    print "\n* Running command '{}'".format(show_cmd)
+    print("\n* Running command '{}'".format(show_cmd))
     cli.executep(show_cmd)
